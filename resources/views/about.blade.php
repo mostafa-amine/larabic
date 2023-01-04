cc88970b4962f32a70924b5b7637ee826c3a466d
mostafa amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2023-01-04 10:21:12 +0100

    ✨ initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="col-md-3 col-sm-6 col-12">
+    <div class="info-box bg-success">
+    <span class="info-box-icon"><i class="far fa-thumbs-up"></i></span>
+    <div class="info-box-content">
+    <span class="info-box-text">Likes</span>
+    <span class="info-box-number">41,410</span>
+    <div class="progress">
+    <div class="progress-bar" style="width: 70%"></div>
+    </div>
+    <span class="progress-description">
+    70% Increase in 30 Days
+    </span>
+    </div>
+
+    </div>
+
+    </div>
+
+
+
+
+@endsection
+
